d84d3cbd6e765b19a490c1653da10d5e8a3c33c3
venom889 <ben@gymtrack.co>
2015-04-07 20:58:28 -0400

    Fixed last login date issue that was messing up tokens

--- src/edu/carleton/comp4601/project/model/UserProfile.java
+++ src/edu/carleton/comp4601/project/model/UserProfile.java
@@ -1,20 +1,34 @@
 package edu.carleton.comp4601.project.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
-public class StatResponse {
+@XmlRootElement(name = "profile")
+@XmlAccessorType (XmlAccessType.FIELD)
+public class UserProfile {
 
+	private String userId;
 	private String upvotes;
 	private String downvotes;
 	private String total;
 	
-	public StatResponse() {
+	public UserProfile() {
 		
 	}
 	
-	public StatResponse(int up, int down, int total) {
+	public UserProfile(int up, int down, int total) {
 		this.upvotes = Integer.toString(up);
 		this.downvotes = Integer.toString(down);
 		this.total = Integer.toString(total);
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
 	}
 
 	public String getUpvotes() {
